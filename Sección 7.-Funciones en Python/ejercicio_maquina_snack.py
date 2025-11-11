c4561f1b5ba6f8b557b6e0570dc8383634638cf6
alejandro05062001 <leandroalejandro2001@gmail.com>
2025-11-11 12:05:22 -0300

    Ejercicio terminado con exito!

--- Sección 7.-Funciones en Python/ejercicio_maquina_snack.py
+++ Sección 7.-Funciones en Python/ejercicio_maquina_snack.py
@@ -2,6 +2,8 @@ snacks = [{"id": 1, "producto": "Papas", "precio": 30},
           {"id": 2, "producto": "Refresco", "precio": 50},
           {"id": 3, "producto": "Sandwich", "precio": 120}]
 
+carrito = []
+
 
 def menu():
     print("*** Maquina de Snacks ***")
@@ -11,23 +13,47 @@ def menu():
             3.- Mostrar ticket
             4.- Salir''')
     opción = int(input("Escoge una opción: "))
+    if opción == 1:
+        mostrar_snacks()
+    elif opción == 2:
+        comprar_snack()
+    elif opción == 3:
+        ticket_de_venta()
+    elif opción == 4:
+        print("Regresa pronto!")
+    else:
+        print("Opción invalida. seleccciona otra opción...")
+        menu()
 
 def mostrar_snacks():
     print("--- Snacks Disponibles ---")
     for snack in snacks:
         print(f"ID: {snack["id"]} -> {snack["producto"]} - Precio {snack["precio"]}")
+    menu()
 
-def mostrar_snack():
-    selección = int(input("Que snack quieres (id)?: "))
-    lista_snack = []
-    for snack in snacks:
-        id_snack = snack["id"]
-        lista_snack.append(id_snack)
-    if selección in lista_snack:
-        print(f"ID: {snacks[selección-1]["id"]} -> {snacks[selección-1]["producto"]} - Precio {snacks[selección-1]["precio"]}")
+
+def comprar_snack():
+    id_disponibles = []
+    for s in snacks:
+        agregar = s["id"]
+        id_disponibles.append(agregar)
+    snack = int(input("Que snack quieres?(id): "))
+    if snack not in id_disponibles:
+        print(f"Snack no encontrado con id = {snack}")
     else:
-        print(f"Snack no encontrado con id = {selección}")
+        añadir = snacks[snack-1]
+        print(f"OK. snack agregado : {añadir}")
+        carrito.append(añadir)
+    menu()
 
-mostrar_snack()
-    
+def ticket_de_venta():
+    print("*** Ticket de Venta ***")
+    total = 0
+    for productos in carrito:
+        print(f"- {productos["producto"]} - ${productos["precio"]}")
+        total += productos["precio"]
+    print(f"TOTAL -> {total}")
+    menu()
 
+
+menu()
